4489ed26befb29ac109d22282e1f257780674da7
azambembem <azamjan980713@gmail.com>
2023-09-26 01:10:48 +0900

    UI: navbar

--- src/App.tsx
+++
@@ -1,7 +0,0 @@
-import type { FC } from "react";
-
-const App: FC = () => {
-  return <div className="App">Welconme to React!</div>;
-};
-
-export default App;
---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,37 @@
+import type { FC } from "react";
+import {
+  LoginOutlined,
+  SearchOutlined,
+  ShoppingCartOutlined,
+  BellOutlined,
+} from "@ant-design/icons";
+
+const Navbar: FC = () => {
+  const icon_style: string = "cursor-pointer text-[20px]";
+  return (
+    <div className="w-[80%] m-auto flex justify-between items-center h-[90px] border-b border-[#46A35880]">
+      <div>
+        <img
+          src="https://firebasestorage.googleapis.com/v0/b/aema-image-upload.appspot.com/o/greenshop%2Ficons%2Flogo.svg?alt=media&token=fc9659d6-f435-43b9-a624-8b0d3a574baa"
+          alt="logo"
+        />
+      </div>
+      <div className="flex gap-4">
+        <h3 className="cursor-pointer">Home</h3>
+        <h3 className="cursor-pointer">Blog</h3>
+        <div />
+        <div className="flex gap-[30px] items-center">
+          <SearchOutlined className={`${icon_style}`} />
+          <BellOutlined className={`${icon_style}`} />
+          <ShoppingCartOutlined className={`${icon_style}`} />
+          <button className="text-white w-[100px] h-[35px] bg-[#46A358] flex justify-center items-center rounded-md gap-2">
+            <LoginOutlined />
+            Login
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
